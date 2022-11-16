ad30e0e6659fbbdca2cc299d7cda06d7b6b91daf
jgimenezbalmes <jgimenez@jaumebalmes.net>
2022-11-16 16:50:14 +0100

    projecte buit

---
+++ src/main/java/APIRest/ApiRestProjecteApplication.java
@@ -0,0 +1,13 @@
+package APIRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestProjecteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestProjecteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demojparelacions/ApiRestProjecteApplicationTests.java
@@ -0,0 +1,13 @@
+package demojparelacions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestProjecteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
