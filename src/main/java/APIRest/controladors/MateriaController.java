fb647344a998f7c3d03b4396c16e5541b1a201ef
JonathanValleA <jvalle@jaumebalmes.net>
2022-11-17 16:55:33 +0100

    Controladores Materias y Professors

--- src/main/java/APIRest/controladors/MateriaController.java
+++ src/main/java/APIRest/controladors/MateriaController.java
@@ -1,5 +1,30 @@
 package APIRest.controladors;
 
-public class MateriaController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import APIRest.entitats.Materia;
+import APIRest.repositoris.MateriaRepositori;
+
+@RestController
+@RequestMapping("api")
+public class MateriaController {
+	
+	@Autowired
+	MateriaRepositori materiaRep;
+	
+    @GetMapping("materias")
+    public Iterable<Materia> getMateria(){
+        return materiaRep.findAll();
+    }
+
+    @GetMapping("materias/{id}")
+    public Materia getMateria(@PathVariable long id) {
+        Materia client = materiaRep.findById(id).get();
+
+        return client;
+    }
 }
--- src/main/java/APIRest/controladors/ProfessorController.java
+++ src/main/java/APIRest/controladors/ProfessorController.java
@@ -1,5 +1,25 @@
 package APIRest.controladors;
 
-public class ProfessorController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import APIRest.entitats.Professor;
+import APIRest.repositoris.ProfessorRepositori;
+
+public class ProfessorController {
+	@Autowired
+	ProfessorRepositori professorRep;
+	
+    @GetMapping("professor")
+    public Iterable<Professor> getProfessor(){
+        return professorRep.findAll();
+    }
+
+    @GetMapping("professor/{id}")
+    public Professor getProfessor(@PathVariable long id) {
+        Professor client = professorRep.findById(id).get();
+
+        return client;
+    }
 }
