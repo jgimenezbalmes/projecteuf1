e5335e428528816c0f92861ab2d49fb03784abe7
jgimenezbalmes <jgimenez@jaumebalmes.net>
2022-11-17 15:56:43 +0100

    actualitzades entitats, afegit controller

--- src/main/java/APIRest/entitats/Materia.java
+++ src/main/java/APIRest/entitats/Materia.java
@@ -1,5 +1,13 @@
 package APIRest.entitats;
 
-public class Materia {
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+public class Materia {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String nomM;
+	private int numeroUFs;
 }
--- src/main/java/APIRest/entitats/Professor.java
+++ src/main/java/APIRest/entitats/Professor.java
@@ -1,5 +1,15 @@
 package APIRest.entitats;
 
-public class Professor {
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+public class Professor {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String nomP;
+	private String cognomP;
+	private String emailP;
+	private boolean teDoctorat;
 }
